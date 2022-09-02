869fac89c97689878e364600677d113ff9a685b9
kawshan <112684462+kawshan@users.noreply.github.com>
2022-09-02 22:12:56 +0530

    question
    question update done need to do binding

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -8,6 +8,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Question[]questions=new Question[]{
       //create question objects
+            new Question(R.string.question_amendments,false),
+            new Question(R.string.question_consitution,true),
+            new Question(R.string.question_declaration,true),
+            new Question(R.string.question_independence_rights,true),
+            new Question(R.string.question_religion,true),
+            new Question(R.string.question_goverment,false),
+            new Question(R.string.question_govenment_feds,false),
+            new Question(R.string.question_governmen_senators,true),
     };
 
     @Override
